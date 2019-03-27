38acb347d6d6b5528cc640779e9e1c617ab91e56
Matej Urbas <matej.urbas@gmail.com>
2019-03-27 18:33:12 +0000

    Implemented github project list downloader.

--- deep_release_notes/cli.py
+++ deep_release_notes/cli.py
@@ -1,18 +1,58 @@
 # -*- coding: utf-8 -*-
 
 """Console script for deep_release_notes."""
-import sys
 import click
+import logging
+from pathlib import Path
+import re
+import requests
+import sys
 
 
-@click.command()
-def main(args=None):
-    """Console script for deep_release_notes."""
-    click.echo(
-        "Replace this message by putting your code into " "deep_release_notes.cli.main"
+@click.group()
+def main():
+    logging.basicConfig(
+        format="[%(levelname)s %(asctime)s|%(filename)s:%(lineno)d] %(message)s",
+        level=logging.INFO,
     )
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+
+
+@main.command(name="get-projects")
+def get_projects():
+    get_projects_since()
+
+
+def get_projects_since():
+    http_session = requests.session()
+    downloaded_files = [str(f) for f in Path().iterdir() if f.is_file()]
+    since_id = find_latest_project_id(downloaded_files) or 0
+    while True:
+        response = http_session.get(
+            "https://api.github.com/repositories", params={"since": since_id}
+        )
+        response.raise_for_status()
+        until_id = response.json()[-1]["id"]
+        write_projects_file(since_id, until_id, response.text)
+        logging.info("Downloaded projects since %s until %s.", since_id, until_id)
+        since_id = until_id
+
+
+def write_projects_file(since_id, until_id, project_list_json_str):
+    projects_file_name = f"projects-{since_id}-{until_id}.json"
+    with open(projects_file_name, "w") as projects_file:
+        projects_file.write(project_list_json_str)
+
+
+def find_latest_project_id(projects_files):
+    if not projects_files:
+        return None
+    pattern = re.compile(r"projects-\d+-(?P<until_id>\d+).json")
+    until_ids = [
+        int(m.group("until_id"))
+        for m in [pattern.match(projects_file) for projects_file in projects_files]
+        if m is not None
+    ]
+    return max(until_ids) if until_ids else None
 
 
 if __name__ == "__main__":
--- tests/test_deep_release_notes.py
+++ tests/test_deep_release_notes.py
@@ -3,36 +3,33 @@
 
 """Tests for `deep_release_notes` package."""
 
-import pytest
-
 from click.testing import CliRunner
-
-from deep_release_notes import deep_release_notes
 from deep_release_notes import cli
+from deep_release_notes.cli import find_latest_project_id
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
+def test_help():
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert "deep_release_notes.cli.main" in result.output
     help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
     assert "--help  Show this message and exit." in help_result.output
+
+
+def test_find_latest_project_id_none():
+    latest_project_id = find_latest_project_id(())
+    assert latest_project_id is None
+
+
+def test_find_latest_project_id_one():
+    latest_project_id = find_latest_project_id(["projects-0-123.json"])
+    assert latest_project_id == 123
+
+
+def test_find_latest_project_id():
+    latest_project_id = find_latest_project_id(["projects-0-123.json", "projects-234-345.json", "projects-123-234.json"])
+    assert latest_project_id == 345
+
+
+def test_find_latest_project_id_incorrect():
+    latest_project_id = find_latest_project_id(["projects--123.json"])
+    assert latest_project_id is None
